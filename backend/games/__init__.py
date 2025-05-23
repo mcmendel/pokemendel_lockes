293f8af1928e70daf74da2853eb7b6bd4130041e
Nir Mendel <mcmendel@gmail.com>
2025-05-23 21:25:57 +0300

    update import games

---
+++ backend/games/__init__.py
@@ -0,0 +1,15 @@
+from .gen1 import Game, BLUE, RED, YELLOW
+from .gen2 import GOLD, SILVER, CRYSTAL
+from typing import List
+GAMES = [BLUE, RED, YELLOW, GOLD, SILVER, CRYSTAL]
+
+
+def get_game(game_name: str) -> Game:
+    relevant_game = next(game for game in GAMES if game.name == game_name)
+    return relevant_game
+
+
+def get_games_from_gen(gen: int) -> List[Game]:
+    return [
+        game for game in GAMES if game.gen >= gen
+    ]
