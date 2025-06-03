4d2502ac07ab2a1fae9987dda541f0d64313cf83
Nir Mendel <mcmendel@gmail.com>
2025-06-03 21:56:59 +0300

    web: add run configuration page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import NewRunPage from './components/NewRunPage';
+import RunConfiguration from './components/RunConfiguration';
 import Layout from './components/Layout';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/locke_manager" element={<HomePage />} />
           <Route path="/locke_manager/new" element={<NewRunPage />} />
+          <Route path="/new/:runName" element={<RunConfiguration />} />
           <Route path="*" element={<Navigate to="/locke_manager" replace />} />
         </Routes>
       </Layout>
