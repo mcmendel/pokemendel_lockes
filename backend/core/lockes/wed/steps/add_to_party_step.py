0036da24e2cfb4139b1149149aeae7c7504924e3
Nir Mendel <mcmendel@gmail.com>
2025-07-29 21:10:59 -0400

    implement wedlocke add to party

--- backend/core/lockes/wed/steps/add_to_party_step.py
+++ backend/core/lockes/wed/steps/add_to_party_step.py
@@ -5,9 +5,18 @@ from definitions.runs.steps_interface import StepInterface, ExecutionReturnValue
 from definitions.runs.inputs_options import InputOptions
 from core.run import Run
 from core.steps.add_to_party_step import AddToPartyStep as AddToPartyStepOg
-from core.lockes.unique.utils import can_pokemon_be_in_party
+from core.lockes.wed.utils import get_party_pairs, get_pokemon_partner
 
 
 class AddToPartyStep(AddToPartyStepOg):
+    def is_step_relevant(self, run: Run, pokemon: Pokemon) -> bool:
+        all_pairs = get_party_pairs(run)
+        return super().is_step_relevant(run, pokemon) and any(pair is None for pair in all_pairs)
 
-    pass
+    def execute_step(self, run: Run, pokemon: Pokemon, value: Optional[str]) -> ExecutionReturnValue:
+        pokemon_partner = get_pokemon_partner(run, pokemon)
+        return_value = super().execute_step(run, pokemon, value)
+        if pokemon_partner:
+            return_value_partner = super().execute_step(run, pokemon_partner, value)
+            return_value.pokemons_to_update.extend(return_value_partner.pokemons_to_update)
+        return return_value
---
+++ backend/core/lockes/wed/utils.py
@@ -0,0 +1,51 @@
+from core.run import Run
+from definitions.pokemons.pokemon import Pokemon
+from dataclasses import dataclass
+from typing import Optional, Tuple, Set
+
+
+@dataclass
+class PairedPokemons:
+    pokemon1: Pokemon
+    pokemon2: Optional[Pokemon]
+
+
+def get_party_pairs(run: Run) -> Tuple[PairedPokemons, Optional[PairedPokemons], Optional[PairedPokemons]]:
+    assert not run.party.is_empty(), "Can't extract pairs from empty party"
+    first_pokemon = run.party.pokemons[0]
+    added_pokemons = {first_pokemon.metadata.id}
+    pair1 = PairedPokemons(pokemon1=first_pokemon)
+    pair2 = None
+    pair3 = None
+
+    _add_partner_to_paired_couple(run, pair1, added_pokemons)
+
+    for party_pokemon in run.party.pokemons[1:]:
+        if party_pokemon.metadata.id in added_pokemons:
+            continue
+
+        if pair2 is None:
+            pair2 = PairedPokemons(pokemon1=party_pokemon)
+            _add_partner_to_paired_couple(run, pair2, added_pokemons)
+        elif pair3 is None:
+            pair3 = PairedPokemons(pokemon1=party_pokemon)
+            _add_partner_to_paired_couple(run, pair3, added_pokemons)
+        else:
+            assert False, "All pairs in party were set. There is a pokemon that is not a part of a pair"
+
+    return pair1, pair2, pair3
+
+
+def _add_partner_to_paired_couple(run: Run, paired_couple: PairedPokemons, added_pokemons: Set[str]):
+    assert paired_couple.pokemon1, "Can add partner to not existing pokemon"
+    assert not paired_couple.pokemon2, "Can't add partner if already added"
+    pokemon_partner = get_pokemon_partner(run, paired_couple.pokemon1)
+    if pokemon_partner:
+        assert run.party.is_pokemon_in_party(pokemon_partner), f"Partner {pokemon_partner.metadata.id} for pokemon {paired_couple.pokemon1.metadata.id} must be in party"
+        added_pokemons.add(pokemon_partner.metadata.id)
+        paired_couple.pokemon2 = pokemon_partner
+
+
+def get_pokemon_partner(run: Run, pokemon: Pokemon) -> Optional[Pokemon]:
+    if pokemon.metadata.paired:
+        return run.box.get_pokemon_by_id(pokemon.metadata.id)
