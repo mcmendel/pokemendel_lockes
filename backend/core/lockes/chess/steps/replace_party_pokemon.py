ddc10168f205bbf57f2e3440e2a26e3376021d96
Nir Mendel <mcmendel@gmail.com>
2025-07-31 04:53:29 -0400

    implement ChessLocke replace pokemon step

--- backend/core/lockes/chess/steps/replace_party_pokemon.py
+++ backend/core/lockes/chess/steps/replace_party_pokemon.py
@@ -1,10 +1,15 @@
-
-from typing import Optional
 from definitions import Pokemon
-from definitions.runs.steps_interface import ExecutionReturnValue
 from core.steps.replace_party_pokemon import ReplacePartyPokemon as ReplacePartyPokemonOg
+from definitions.runs.inputs_options import InputOptions
 from core.run import Run
+from core.lockes.chess.utils import get_party_roles
+from typing import Tuple, List
 
 
 class ReplacePartyPokemon(ReplacePartyPokemonOg):
-    pass
+    def step_options(self, run: Run, pokemon: Pokemon) -> Tuple[InputOptions, List[str]]:
+        party_roles = get_party_roles(run)
+        if pokemon.metadata.chesslocke_role in party_roles:
+            same_role_party = [pokemon.metadata.id for pokemon in run.party.pokemons if pokemon.metadata.chesslocke_role == pokemon.metadata.chesslocke_role]
+            return InputOptions.ONE_OF, same_role_party
+        return super().step_options(run, pokemon)
