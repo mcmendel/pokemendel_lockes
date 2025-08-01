5d306463f0bcbd0432c2c40796b74f221ed22806
Nir Mendel <mcmendel@gmail.com>
2025-08-01 05:52:08 -0400

    implement WrapLocke replace pokemon step

--- backend/core/lockes/wrap/steps/replace_party_pokemon.py
+++ backend/core/lockes/wrap/steps/replace_party_pokemon.py
@@ -1,12 +1,21 @@
 
-from typing import Tuple, List, Optional
+from typing import Tuple, List
 from definitions import Pokemon
-from definitions.runs.steps_interface import StepInterface, ExecutionReturnValue
 from definitions.runs.inputs_options import InputOptions
 from core.steps.replace_party_pokemon import ReplacePartyPokemon as ReplacePartyPokemonOg
-from core.lockes.unique.utils import get_party_pokemons_with_intersected_types
+from core.lockes.wrap.utils import get_sorted_party
 from core.run import Run
 
 
 class ReplacePartyPokemon(ReplacePartyPokemonOg):
-    pass
+    def is_step_relevant(self, run: Run, pokemon: Pokemon) -> bool:
+        return super().is_step_relevant(run, pokemon) and len(run.party.pokemons) > 4
+
+    def step_options(self, run: Run, pokemon: Pokemon) -> Tuple[InputOptions, List[str]]:
+        sorted_party = get_sorted_party(run)
+        assert len(sorted_party) >= 4, "Can't replace wrap pokemons. Must have at least one meat pokemon"
+        relevant_party_members = [
+            party_pokemon.metadata.id
+            for party_pokemon in sorted_party[2:-2]
+        ]
+        return InputOptions.ONE_OF, relevant_party_members
