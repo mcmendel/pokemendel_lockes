cb418e8a4ff0b8fd276b25a620fb08689b1a303a
mcmendel <32415687+mcmendel@users.noreply.github.com>
2025-08-10 13:49:11 -0400

    add castform locke (#47)

---
+++ backend/tests/e2e/gen3_helpers.py
@@ -0,0 +1,11 @@
+from pokemendel_core.data.gen3 import PokemonGen3
+
+
+GAME_NAME = "Emerald"
+NUM_POKEMONS = 386
+NUM_ENCOUNTERS = 0
+
+STARTERS = {PokemonGen3.TORCHIC, PokemonGen3.TREECKO, PokemonGen3.MUDKIP}
+
+# RED_NUM_ENCOUNTER = 89
+# BLUE_NUM_ENCOUNTER = 93
