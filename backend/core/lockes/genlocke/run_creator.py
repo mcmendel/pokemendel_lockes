a8a90d05e32061ce963815da006332b753a6b001
Nir Mendel <mcmendel@gmail.com>
2025-08-13 08:25:42 -0400

    instead of pre-deciding all games, genlocke will do it when finishing current run

--- backend/core/lockes/genlocke/run_creator.py
+++ backend/core/lockes/genlocke/run_creator.py
@@ -1,7 +1,7 @@
 from core.lockes.base.run_creator import RunCreator, RunCreationProgress, List
 from core.lockes.lockes_factory import list_all_lockes, GenLocke, LOCKE_INSTANCES
 from core.lockes.run_creation_factory import get_run_creator_class
-from games import Game, get_games_from_gen
+from games import Game, get_games_from_gen, get_game
 from pokemendel_core.utils.definitions.regions import Regions
 
 
@@ -14,14 +14,14 @@ _GEN_TO_REGION = {
     3: Regions.HOENN,
 }
 
+_REGION_TO_GEN = {region: gen for gen, region in _GEN_TO_REGION.items()}
+
 
 class GenRunCreator(RunCreator):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._internal_run_creator = None
-        if self.run_creation and _SELECTED_LOCKE in self.run_creation.extra_info:
-            creator_cls = get_run_creator_class(self.run_creation.extra_info[_SELECTED_LOCKE])
-            self._internal_run_creator = creator_cls(self.run_creation)
+        self._init_internal_run_creation()
     def _get_unfinished_progress(self, locke_min_gen: int) -> RunCreationProgress:
         if _SELECTED_LOCKE not in self.run_creation.extra_info:
             return RunCreationProgress(
@@ -30,22 +30,10 @@ class GenRunCreator(RunCreator):
                 missing_key=_SELECTED_LOCKE,
                 missing_key_options=[locke for locke in list_all_lockes() if locke != GenLocke.name]
             )
-
-        for supported_gens in sorted(_GEN_TO_REGION.keys()):
-            if f"{_GAME_PREFIX}{supported_gens}" in self.run_creation.extra_info:
-                if supported_gens == 1:
-                    self.run_creation.game = self.run_creation.extra_info[f"{_GAME_PREFIX}{supported_gens}"]
-                continue
-            gen_games = self._get_games_from_gen(supported_gens)
-            if not gen_games:
-                continue
-            return RunCreationProgress(
-                self.run_creation,
-                has_all_info=False,
-                missing_key=f"{_GAME_PREFIX}{supported_gens}",
-                missing_key_options=[game.name for game in gen_games]
-            )
-        return self._internal_run_creator.get_progress(locke_min_gen)
+        self._init_internal_run_creation()
+        locke = LOCKE_INSTANCES[self.run_creation.extra_info[_SELECTED_LOCKE]]
+        creation_progress = self._internal_run_creator.get_progress(locke.min_gen)
+        return creation_progress
 
     def _get_games_from_gen(self, locke_min_gen: int) -> List[Game]:
         locke_name = self.run_creation.extra_info[_SELECTED_LOCKE]
@@ -54,3 +42,8 @@ class GenRunCreator(RunCreator):
             game for game in get_games_from_gen(locke.min_gen)
             if game.region == _GEN_TO_REGION[locke_min_gen]
         ]
+
+    def _init_internal_run_creation(self):
+        if not self._internal_run_creator and self.run_creation and _SELECTED_LOCKE in self.run_creation.extra_info:
+            creator_cls = get_run_creator_class(self.run_creation.extra_info[_SELECTED_LOCKE])
+            self._internal_run_creator = creator_cls(self.run_creation)
--- backend/tests/e2e/test_genlocke.py
+++ backend/tests/e2e/test_genlocke.py
@@ -40,22 +40,23 @@ _GEN_TO_GAME = {
 
 
 @pytest.mark.parametrize("locke,extra_info,relevant_games_gens", [
-    (BaseLocke.name, False, [1, 2, 3]),
-    (CastformLocke.name, False, [1, 3]),
-    (CategoryLocke.name, True, [1, 2, 3]),
-    (ChessLocke.name, False, [1, 2, 3]),
-    (ColorLocke.name, True, [1, 2, 3]),
+    (BaseLocke.name, False, [2, 3]),
+    (CastformLocke.name, False, [3]),
+    (CategoryLocke.name, True, [2, 3]),
+    (ChessLocke.name, False, [2, 3]),
+    (ColorLocke.name, True, [2, 3]),
     (DeoxysLocke.name, False, [1, 3]),
-    (EeveeLocke.name, False, [1, 2, 3]),
-    (LegLocke.name, True, [1, 2, 3]),
-    (MonoLocke.name, True, [1, 2, 3]),
-    (StarLocke.name, True, [1, 2, 3]),
-    (UniqueLocke.name, False, [1, 2, 3]),
-    (WedLocke.name, False, [1, 2, 3]),
-    (WrapLocke.name, False, [1, 2, 3]),
+    (EeveeLocke.name, False, [2, 3]),
+    (LegLocke.name, True, [2, 3]),
+    (MonoLocke.name, True, [2, 3]),
+    (StarLocke.name, True, [2, 3]),
+    (UniqueLocke.name, False, [2, 3]),
+    (WedLocke.name, False, [2, 3]),
+    (WrapLocke.name, False, [2, 3]),
 ])
 def test_base_gen(client_fixture, locke, extra_info, relevant_games_gens):
     run_id = _create_run(client_fixture, GEN3_GAME_NAME, locke, extra_info, relevant_games_gens)
+    assert run_id
 
 
 def _create_run(client_fixture, game_name, locke, extra_info, relevant_games_gens):
@@ -64,26 +65,12 @@ def _create_run(client_fixture, game_name, locke, extra_info, relevant_games_gen
     with pytest.raises(AssertionError, match=f"Failed to continue run creation with game {game_name}"):
         start_locke_creation(client_fixture, TEST_LOCKE, game_name, True, False)
 
-    last_gen = max(_GEN_TO_REGION.keys())
     continue_locke_creation_not_finished(client_fixture, None, None, '_selected_locke', locke)
-    continue_locke_creation_not_finished(client_fixture, '_selected_locke', locke, "_game_1", "Fire Red")
-    for idx, relevant_gen in enumerate(relevant_games_gens):
-        assert relevant_gen <= last_gen
-        if relevant_gen < last_gen:
-            next_gen = relevant_games_gens[idx + 1]
-            continue_locke_creation_not_finished(
-                client_fixture,
-                f"_game_{relevant_gen}", _GEN_TO_GAME[relevant_gen],
-                f"_game_{next_gen}", _GEN_TO_GAME[next_gen],
-            )
-            continue
-        if extra_info:
-            continue_locke_creation_not_finished(
-                client_fixture, f"_game_{last_gen}", _GEN_TO_GAME[last_gen], None, None
-            )
-        else:
-            return continue_locke_creation_finished(client_fixture, f"_game_{last_gen}", _GEN_TO_GAME[last_gen])
-    return None
+    continue_locke_creation_not_finished(client_fixture, '_selected_locke', locke, "GAME", "Fire Red")
+    if extra_info:
+        continue_locke_creation_not_finished(client_fixture, "GAME", "Fire Red", None, None)
+        return "run_id"
+    return continue_locke_creation_finished(client_fixture, "GAME", "Fire Red")
 
 
 if __name__ == '__main__':
